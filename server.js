d49942c3320db24fa074d5d777e073b7424e07e2
Nguyễn Văn Minh Tú <134575525+tuzx0033@users.noreply.github.com>
2025-12-12 11:54:54 +0700

    baitap1

---
+++ server.js
@@ -0,0 +1,14 @@
+const app = require("./src/app");
+
+const PORT = 3055
+
+
+
+const server = app.listen(PORT, () => { 
+    console.log('eCommerce start with port', PORT);
+});
+
+
+process.on('SIGINT', () => {
+    server.close(()=> console.log('exit'))
+}) 
--- src/app.js
+++ src/app.js
@@ -1,2 +1,38 @@
 const express = require('express');
+const { default: helmet } = require('helmet')
+const compression = require('compression');
+const morgan = require('morgan');
+const app = express()
+
+
+//init middlewares
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression())
+
+
+// morgan("compile")
+// morgan("commom")
+// morgan("short")
+// morgan("tiny")
+// morgan("dev")
+
+
+// test route
+
+app.get('/', (req, res, next) => {
+    const strCompress = 'hello form minh tu'
+    return res.status(200).json({
+        message: 'xinchao',
+        metadata: strCompress.repeat(22323230)
+    })
+});
+
+//init db
+
+
+//init routes
+
+
+module.exports = app;
 
